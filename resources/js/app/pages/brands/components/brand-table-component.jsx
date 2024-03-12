1a07c06d7515b6199545579031a326f2550c1a98
EOGSqwerty <scitdept@empireonegroup.com>
2024-03-12 20:39:22 +0800

    Change edit button icon

--- resources/js/app/pages/brands/components/brand-table-component.jsx
+++ resources/js/app/pages/brands/components/brand-table-component.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function BrandTableComponent({name}) {
     return (
         <>
-            <th scope="col" className="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <th scope="col" className="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400 justify-center items-center">
                 {name}
             </th>
         </>
