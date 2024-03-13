4fff06e18e89de68febf3d38ea91eee46792fa16
marlou <gb.bakeshop.app@gmail.com>
2024-03-14 00:58:26 +0800

    table button action adjustment padding

--- resources/js/app/pages/item_types/sections/itemtypes-table-section.jsx
+++ resources/js/app/pages/item_types/sections/itemtypes-table-section.jsx
@@ -59,7 +59,7 @@ export default function ItemtypeTableSection() {
                                             <th scope="col" className="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400 justify-center items-center">
                                                 Name
                                             </th>
-                                            <th scope="col" className="px-16 py-3.5 text-sm font-normal text-right rtl:text-right text-gray-500 dark:text-gray-400 justify-center items-center">
+                                            <th scope="col" className="px-12 py-3.5 text-sm font-normal text-right rtl:text-right text-gray-500 dark:text-gray-400 justify-center items-center">
                                                 Action
                                             </th>
                                         </tr>
@@ -77,8 +77,8 @@ export default function ItemtypeTableSection() {
                                                 </div>
                                             </td>
                                             <td className="px-4 text-sm whitespace-nowrap flex items-center justify-end gap-2 py-2">
-                                            <button type="button" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-5 py-2 text-center"><PencilSquareIcon className='h-6 text-white' /></button>
-                                                <button type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2 text-center"><TrashIcon className='h-6 text-white' /></button>
+                                            <button type="button" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-3 py-2 text-center"><PencilSquareIcon className='h-6 text-white' /></button>
+                                                <button type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-3 py-2 text-center"><TrashIcon className='h-6 text-white' /></button>
                                             </td>
                                         </tr>
 
