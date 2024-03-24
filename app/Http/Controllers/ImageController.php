1971c5456e2495d341c6ccf9fcf25a1d08c3bf32
marlou <gb.bakeshop.app@gmail.com>
2024-03-25 00:59:17 +0800

    revert changes

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Image;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -11,12 +10,7 @@ class ImageController extends Controller
     public function store(Request $request)
     {
         $file = $request->file('file')->store('images');
-        $url = Storage::disk('s3')->url($file); 
-
-        Image::create([
-            'title'=>$request->title,
-            'path'=>$url
-        ]);
+        $url = Storage::disk('s3')->url($file); // Get full URL of the uploaded file
         return response()->json([
             'url' => $url
         ], 200);
