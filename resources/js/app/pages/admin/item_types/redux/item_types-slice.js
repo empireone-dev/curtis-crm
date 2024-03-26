2ddf645e3a5f983e360d6403661e1ad9944e32a2
EOGSqwerty <scitdept@empireonegroup.com>
2024-03-26 18:37:21 +0800

    fetch data for common issue table

--- resources/js/app/pages/admin/item_types/redux/item_types-slice.js
+++ resources/js/app/pages/admin/item_types/redux/item_types-slice.js
@@ -3,15 +3,15 @@ import { createSlice } from '@reduxjs/toolkit'
 export const itemTypesSlice = createSlice({
   name: 'item_types',
   initialState: {
-    item_types: [] 
+    item_types: []
   },
   reducers: {
     setItemTypes: (state, action) => {
-      state.item_types = action.payload 
+      state.item_types = action.payload
     },
   },
 })
 
-export const { setItemTypes } = itemTypesSlice.actions 
+export const { setItemTypes } = itemTypesSlice.actions
 
 export default itemTypesSlice.reducer 
