c1293478c0560391aba521f9094a8eea1cb86e0b
unknown <Marlou Dev>
2024-05-20 02:50:18 +0800

    added vendors

---
+++ vendor/jeremy-dunn/php-fedex-api-wrapper/src/FedEx/OpenShipService/ComplexType/Payment.php
@@ -0,0 +1,62 @@
+<?php
+namespace FedEx\OpenShipService\ComplexType;
+
+use FedEx\AbstractComplexType;
+
+/**
+ * Payment
+ *
+ * @author      Jeremy Dunn <jeremy@jsdunn.info>
+ * @package     PHP FedEx API wrapper
+ * @subpackage  OpenShip Service
+ *
+ * @property \FedEx\OpenShipService\SimpleType\PaymentType|string $PaymentType
+ * @property Payor $Payor
+ * @property EPaymentDetail $EPaymentDetail
+
+ */
+class Payment extends AbstractComplexType
+{
+    /**
+     * Name of this complex type
+     *
+     * @var string
+     */
+    protected $name = 'Payment';
+
+    /**
+     * Set PaymentType
+     *
+     * @param \FedEx\OpenShipService\SimpleType\PaymentType|string $paymentType
+     * @return $this
+     */
+    public function setPaymentType($paymentType)
+    {
+        $this->values['PaymentType'] = $paymentType;
+        return $this;
+    }
+
+    /**
+     * Set Payor
+     *
+     * @param Payor $payor
+     * @return $this
+     */
+    public function setPayor(Payor $payor)
+    {
+        $this->values['Payor'] = $payor;
+        return $this;
+    }
+
+    /**
+     * FOR FEDEX INTERNAL USE ONLY
+     *
+     * @param EPaymentDetail $ePaymentDetail
+     * @return $this
+     */
+    public function setEPaymentDetail(EPaymentDetail $ePaymentDetail)
+    {
+        $this->values['EPaymentDetail'] = $ePaymentDetail;
+        return $this;
+    }
+}
---
+++ vendor/jeremy-dunn/php-fedex-api-wrapper/src/FedEx/ValidationAvailabilityAndCommitmentService/SimpleType/UploadDocumentType.php
@@ -0,0 +1,23 @@
+<?php
+namespace FedEx\ValidationAvailabilityAndCommitmentService\SimpleType;
+
+use FedEx\AbstractSimpleType;
+
+/**
+ * UploadDocumentType
+ *
+ * @author      Jeremy Dunn <jeremy@jsdunn.info>
+ * @package     PHP FedEx API wrapper
+ * @subpackage  Validation Availability And Commitment Service Service
+ */
+class UploadDocumentType extends AbstractSimpleType
+{
+    const _CERTIFICATE_OF_ORIGIN = 'CERTIFICATE_OF_ORIGIN';
+    const _COMMERCIAL_INVOICE = 'COMMERCIAL_INVOICE';
+    const _ETD_LABEL = 'ETD_LABEL';
+    const _NET_RATE_SHEET = 'NET_RATE_SHEET';
+    const _OTHER = 'OTHER';
+    const _PRO_FORMA_INVOICE = 'PRO_FORMA_INVOICE';
+    const _USMCA_CERTIFICATION_OF_ORIGIN = 'USMCA_CERTIFICATION_OF_ORIGIN';
+    const _USMCA_COMMERCIAL_INVOICE_CERTIFICATION_OF_ORIGIN = 'USMCA_COMMERCIAL_INVOICE_CERTIFICATION_OF_ORIGIN';
+}
