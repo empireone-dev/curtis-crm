e9c266b6a051983eac0f074ecaa0fa7a62067826
unknown <Marlou Dev>
2024-05-22 23:50:53 +0800

    dajkl;dl;kjawdwa

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     build: {
-        chunkSizeWarningLimit: 100000, // Adjust chunk size warning limit if needed
+        chunkSizeWarningLimit: 100000000000, // Adjust chunk size warning limit if needed
     },
     plugins: [
         laravel({
