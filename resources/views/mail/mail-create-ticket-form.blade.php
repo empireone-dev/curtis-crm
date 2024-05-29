b5d53c965a811c3aa64437c9699a9394f0cae037
unknown <Marlou Dev>
2024-05-29 21:30:44 +0800

    fixing mail create

--- resources/views/mail/mail-create-ticket-form.blade.php
+++ resources/views/mail/mail-create-ticket-form.blade.php
@@ -51,7 +51,7 @@
                                 </div>
                                 <div class="px-4 py-2 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-0">
                                     <dt class="text-sm font-medium leading-6 text-gray-900">Track Activity</dt>
-                                    <a target="_blank" href="https://curtis-staging.cloud/customer/ticket-activity/{{ $data['id'] }}" class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">https://curtis-staging.cloud/customer/ticket-activity/{{ $data['id'] }}</a>
+                                    <a target="_blank" href="https://curtis-staging.cloud/customer/tickets?page={{ $data['id'] }}" class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">https://curtis-staging.cloud/customer/ticket-activity/{{ $data['id'] }}</a>
                                 </div>
 
                             </dl>
