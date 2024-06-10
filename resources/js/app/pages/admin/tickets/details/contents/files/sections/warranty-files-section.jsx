49edbf66513b9d1e05426af1dfc9c19c2f1b3859
unknown <Marlou Dev>
2024-06-11 04:17:45 +0800

    additional adjustment

--- resources/js/app/pages/admin/tickets/details/contents/files/sections/warranty-files-section.jsx
+++ resources/js/app/pages/admin/tickets/details/contents/files/sections/warranty-files-section.jsx
@@ -73,19 +73,25 @@ export default function WarrantyFilesSection() {
                 <>
                     <CustomerTicketsBillOfSaleSection />
                     <div className="h-px my-8 border border-blue-500 w-full" />
-                    <CustomerTicketsFrontOfTheUnitSection />
+                    {/* <CustomerTicketsFrontOfTheUnitSection />
                     <div className="h-px my-8 border border-blue-500 w-full" />
                     <CustomerTicketsRearOfTheUnitSection />
                     <div className="h-px my-8 border border-blue-500 w-full" />
                     <CustomerTicketsReadableSerialSection />
                     <div className="h-px my-8 border border-blue-500 w-full" />
-                    <CustomerTicketsDefectIssueSection />
+                    <CustomerTicketsDefectIssueSection /> */}
                 </>
             );
         } else {
             return (
                 <>
-               
+                    {/* <CustomerTicketsSerialModel />
+                    <div className="h-px my-8 border border-blue-500 w-full" />
+                    <CustomerTicketsReceiptModel />
+                    <div className="h-px my-8 border border-blue-500 w-full" />
+                    <CustomerTicketsPartsModel />
+                    <div className="h-px my-8 border border-blue-500 w-full" />
+                    <CustomerTicketsClearModel /> */}
                 </>
             );
         }
