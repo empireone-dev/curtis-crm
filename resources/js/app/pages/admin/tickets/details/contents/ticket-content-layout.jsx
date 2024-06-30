473cac5b3d96374bf310a443100c4c0c1d0a62c0
unknown <Marlou Dev>
2024-06-30 20:56:20 +0800

    added more fixes

--- resources/js/app/pages/admin/tickets/details/contents/ticket-content-layout.jsx
+++ resources/js/app/pages/admin/tickets/details/contents/ticket-content-layout.jsx
@@ -12,21 +12,18 @@ import { setFilesData } from "@/app/pages/customer/tickets/redux/customer-ticket
 import { useDispatch, useSelector } from "react-redux";
 import { get_tickets_by_ticket_id } from "@/app/services/tickets-service";
 import { setTicket } from "../../_redux/tickets-slice";
-import { InboxStackIcon, TicketIcon } from "@heroicons/react/24/outline";
+import { TicketIcon } from "@heroicons/react/24/outline";
 import ContentsRepairPage from "../contents/repair/page";
 import ContentsRefundPage from "../contents/refund/page";
 import ContentsReplacementPartsPage from "../contents/replacement_parts/page";
 import ContentsWarrantyValidationPage from "../contents/warranty_validation/page";
 import WarehousePage from "../contents/warehouse/page";
-import TicketsDetailsMoveAssignComponents from "../components/tickets-details-move-assign-components";
 import TicketsPartsInternalsContent from "../contents/internals/page";
 import TicketsAvailabilityContent from "../contents/availability/page";
 import ContentsCallBackPage from "../contents/call_back/page";
-import ReplacementWarranty from "../contents/replacement_warranty/page";
 import ReplacementWarrantyPage from "../contents/replacement_warranty/page";
 import Skeleton from "@/app/layouts/components/skeleton";
 import TicketsDetailsMoveAssignSection from "../sections/tickets-details-move-assign-section,";
-
 
 export default function TicketsDetailsLayout({ children }) {
     const { ticket } = useSelector((state) => state.tickets);
@@ -36,29 +33,24 @@ export default function TicketsDetailsLayout({ children }) {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-      async function fetchData(params) {
-        try {
-            const res = await store.dispatch(
-                get_upload_ticket_files_thunk(
-                    url.split("/")[url.split("/").length - 2].split("#")[0]
-                )
-            );
-            const ress = await get_tickets_by_ticket_id(
-                url.split("/")[url.split("/").length - 2].split("#")[0]
-            );
-            dispatch(setTicket(ress));
-            dispatch(setFilesData(res));
-            setLoading(false);
-        } catch (error) {
-            setLoading(false);
-            console.error("Error fetching data:", error);
+        async function fetchData() {
+            try {
+                const ticketId = url.split("/")[url.split("/").length - 2].split("#")[0];
+                const res = await store.dispatch(get_upload_ticket_files_thunk(ticketId));
+                const ress = await get_tickets_by_ticket_id(ticketId);
+                dispatch(setTicket(ress));
+                dispatch(setFilesData(res));
+                setLoading(false);
+            } catch (error) {
+                setLoading(false);
+                console.error("Error fetching data:", error);
+            }
         }
-       }
         fetchData();
-    }, [url]);
+    }, [url, dispatch]);
 
     const tabs = [
-        ...(ticket.call_type != "TS-Tech Support"
+        ...(ticket.call_type !== "TS-Tech Support"
             ? [
                   {
                       title: "Files",
@@ -68,7 +60,7 @@ export default function TicketsDetailsLayout({ children }) {
             : []),
         ...(ticket?.isUploading === "true" &&
         ticket.call_type === "CF-Warranty Claim" &&
-        ticket.status == "WARRANTY VALIDATION"
+        ticket.status === "WARRANTY VALIDATION"
             ? [
                   {
                       title: "Warranty Validation",
@@ -101,8 +93,7 @@ export default function TicketsDetailsLayout({ children }) {
         (ticket.status === "CA WAREHOUSE" ||
             ticket.status === "US WAREHOUSE" ||
             ticket.status === "CLOSED")
-            ? 
-              [
+            ? [
                   {
                       title: ticket.country + " Warehouse",
                       components: <WarehousePage />,
@@ -146,7 +137,6 @@ export default function TicketsDetailsLayout({ children }) {
                   },
               ]
             : []),
-
         ...(ticket?.isUploading === "true" && ticket.status === "REFUND"
             ? [
                   {
@@ -165,7 +155,6 @@ export default function TicketsDetailsLayout({ children }) {
                   },
               ]
             : []),
-
         ...(ticket?.isUploading === "true" &&
         ticket.status === "REPLACEMENT PARTS"
             ? [
@@ -176,8 +165,7 @@ export default function TicketsDetailsLayout({ children }) {
                   },
               ]
             : []),
-
-        ...(ticket.call_type == "TS-Tech Support" &&
+        ...(ticket.call_type === "TS-Tech Support" &&
         ticket.status === "TECH VALIDATION"
             ? [
                   {
@@ -187,7 +175,6 @@ export default function TicketsDetailsLayout({ children }) {
                   },
               ]
             : []),
-
         {
             title: "Activities",
             components: <TicketsDetailsContentActivities />,
@@ -210,37 +197,35 @@ export default function TicketsDetailsLayout({ children }) {
     };
     const hash = window.location.pathname.split("/")[5];
     return (
-        <div className=" font-sans h-full">
-            {
-                user.agent_type !== 'CSR' && <TicketsDetailsMoveAssignSection />
-            }
-          
+        <div className="font-sans h-full">
+            {user.agent_type !== "CSR" && <TicketsDetailsMoveAssignSection />}
             <div className="px-8">
                 <div className="w-full ">
                     <div
                         className={`py-3 text-3xl font-black flex gap-3 ${
-                            ticket.status == "CLOSED"
-                                ? " text-red-600"
-                                : " text-blue-600"
+                            ticket.status === "CLOSED"
+                                ? "text-red-600"
+                                : "text-blue-600"
                         }`}
                     >
                         <TicketIcon className="h-9" />{" "}
                         {ticket.status ?? "Open Ticket"} ({ticket.call_type})
                     </div>
                     <div className="mb-4 flex space-x-4 p-2 bg-white rounded-md border-blue-500 border-2 ">
-                        {!loading && tabs.map((res, i) => (
-                            <button
-                                key={i}
-                                onClick={() => handleTabClick(i)}
-                                className={`flex-1 py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-blue transition-all duration-300 ${
-                                    hash == res.hash
-                                        ? "bg-blue-600 text-white"
-                                        : "bg-gray-300 hover:bg-blue-200"
-                                }`}
-                            >
-                                {res.title}
-                            </button>
-                        ))}
+                        {!loading &&
+                            tabs.map((res, i) => (
+                                <button
+                                    key={i}
+                                    onClick={() => handleTabClick(i)}
+                                    className={`flex-1 py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-blue transition-all duration-300 ${
+                                        hash === res.hash
+                                            ? "bg-blue-600 text-white"
+                                            : "bg-gray-300 hover:bg-blue-200"
+                                    }`}
+                                >
+                                    {res.title}
+                                </button>
+                            ))}
                     </div>
                     <div className="my-8">
                         {loading ? <Skeleton /> : children}
