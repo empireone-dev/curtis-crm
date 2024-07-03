79b0c64092c70f9132fd092a72a7a33d86f965dd
unknown <Marlou Dev>
2024-07-04 00:24:25 +0800

    added build

--- resources/js/app/pages/agent/direct_emails/id/sections/log-case-section.jsx
+++ resources/js/app/pages/agent/direct_emails/id/sections/log-case-section.jsx
@@ -6,7 +6,7 @@ import { set_cases_log } from "@/app/pages/admin/users/redux/users-slice";
 
 export default function AgentLogCaseSection({ ticket_id, account }) {
     const [data, setData] = useState({});
-  
+
     const [loading, setLoading] = useState(false);
     const dispatch = useDispatch();
     async function submit_case_log(params) {
@@ -15,9 +15,9 @@ export default function AgentLogCaseSection({ ticket_id, account }) {
             ...data,
             ticket_id: ticket_id,
             user_id: account.id,
-            log_from:'direct_emails'
+            log_from: "direct_emails",
         });
-        setData({})
+        setData({});
         dispatch(set_cases_log(res.data));
         setLoading(false);
     }
@@ -25,7 +25,6 @@ export default function AgentLogCaseSection({ ticket_id, account }) {
         <div>
             <div class="text-gray-600 mb-4 mt-3">
                 <p class="font-medium text-lg">Log Case</p>
-               
 
                 <div class="mt-2">
                     <label>Remarks</label>
@@ -74,45 +73,124 @@ export default function AgentLogCaseSection({ ticket_id, account }) {
                         <option selected disabled>
                             Select
                         </option>
-                        <option value="Pending-Incomplete Details">
-                            Pending-Incomplete Details
-                        </option>
-                        <option value="Pending-Completed and Validated">
-                            Pending-Completed and Validated
-                        </option>
-                        <option value="Pending-Warranty Decision-Refund">
-                            Pending-Warranty Decision-Refund
-                        </option>
-                        <option value="Pending-Warranty Decision-Replacement">
-                            Pending-Warranty Decision-Replacement
-                        </option>
-                        <option value="Pending-Warranty Decision-Repair">
-                            Pending-Warranty Decision-Repair
-                        </option>
-                        <option value="Pending-Case Follow-up">
-                            Pending-Case Follow-up
-                        </option>
-                        <option value="Pending-Undelivered Email / Bounced Back Email">
-                            Pending-Undelivered Email / Bounced Back Email
-                        </option>
-                        <option value="Pending-Proactive Follow-up">
-                            Pending-Proactive Follow-up
-                        </option>
-                        <option value="Case Closed-Warranty Denied">
-                            Case Closed-Warranty Denied
-                        </option>
-                        <option value="Case Closed-Refer to Parts">
-                            Case Closed-Refer to Parts
-                        </option>
-                        <option value="Case Endorsed to TCH">
-                            Case Endorsed to TCH
-                        </option>
-                        <option value="Case Survey Sent">
-                            Case Survey Sent
-                        </option>
-                        <option value="Case Closed-No Survey">
-                            Case Closed-No Survey
-                        </option>
+                        {account.agent_type == "Warranty" && (
+                            <>
+                                <option value="Pending-Incomplete Details">
+                                    Pending-Incomplete Details
+                                </option>
+                                <option value="Pending-Completed and Validated">
+                                    Pending-Completed and Validated
+                                </option>
+                                <option value="Pending-Warranty Decision-Refund">
+                                    Pending-Warranty Decision-Refund
+                                </option>
+                                <option value="Pending-Warranty Decision-Replacement">
+                                    Pending-Warranty Decision-Replacement
+                                </option>
+                                <option value="Pending-Warranty Decision-Repair">
+                                    Pending-Warranty Decision-Repair
+                                </option>
+                                <option value="Pending-Case Follow-up">
+                                    Pending-Case Follow-up
+                                </option>
+                                <option value="Pending-Undelivered Email / Bounced Back Email">
+                                    Pending-Undelivered Email / Bounced Back
+                                    Email
+                                </option>
+                                <option value="Pending-Proactive Follow-up">
+                                    Pending-Proactive Follow-up
+                                </option>
+                                <option value="Case Closed-Warranty Denied">
+                                    Case Closed-Warranty Denied
+                                </option>
+                                <option value="Case Closed-Refer to Parts">
+                                    Case Closed-Refer to Parts
+                                </option>
+                                <option value="Case Endorsed to TCH">
+                                    Case Endorsed to TCH
+                                </option>
+                                <option value="Case Survey Sent">
+                                    Case Survey Sent
+                                </option>
+                                <option value="Case Closed-No Survey">
+                                    Case Closed-No Survey
+                                </option>
+                            </>
+                        )}
+                        
+                        {account.agent_type == "Parts" && (
+                            <>
+                                <option value="Pending - Incomplete Information">
+                                    Pending - Incomplete Information
+                                </option>
+                                <option value="Pending - Part in Order">
+                                    Pending - Part in Order
+                                </option>
+                                <option value="Pending - Case Follow Up">
+                                    Pending - Case Follow Up
+                                </option>
+                                <option value="Pending - Shipping Info (IW)">
+                                    Pending - Shipping Info (IW)
+                                </option>
+                                <option value="Pending - Shipping Info (OOW)">
+                                    Pending - Shipping Info (OOW)
+                                </option>
+                                <option value="Pending - Escalated Case">
+                                    Pending - Escalated Case
+                                </option>
+                                <option value="Responded - Parts Available">
+                                    Responded - Parts Available
+                                </option>
+                                <option value="Responded - Parts Not Available">
+                                    Responded - Parts Not Available
+                                </option>
+                                <option value="Responded - Sent Email">
+                                    Responded - Sent Email
+                                </option>
+                                <option value="Referred to Store">
+                                    Referred to Store
+                                </option>
+                                <option value="Payment Method">
+                                    Payment Method
+                                </option>
+                                <option value="Checking Availability">
+                                    Checking Availability
+                                </option>
+                                <option value="Case Closed - Shipping Damage">
+                                    Case Closed - Shipping Damage
+                                </option>
+                                <option value="Case Closed - Forwarded to Warranty">
+                                    Case Closed - Forwarded to Warranty
+                                </option>
+                                <option value="Case Closed - Forwarded to Tech">
+                                    Case Closed - Forwarded to Tech
+                                </option>
+                                <option value="Case Closed - Customer Not Interested">
+                                    Case Closed - Customer Not Interested
+                                </option>
+                                <option value="Case Closed - No Response (14 days)">
+                                    Case Closed - No Response (14 days)
+                                </option>
+                                <option value="Case Closed - Not Willing to Buy">
+                                    Case Closed - Not Willing to Buy
+                                </option>
+                                <option value="Case Closed - Parts Not Available">
+                                    Case Closed - Parts Not Available
+                                </option>
+                                <option value="Case Closed - Has Existing Ticket">
+                                    Case Closed - Has Existing Ticket
+                                </option>
+                                <option value="Case Closed - Resolved">
+                                    Case Closed - Resolved
+                                </option>
+                                <option value="Case Closed - Survey Sent">
+                                    Case Closed - Survey Sent
+                                </option>
+                                <option value="Case Closed - No Survey">
+                                    Case Closed - No Survey
+                                </option>
+                            </>
+                        )}
                     </select>
                 </div>
                 {/* <button
