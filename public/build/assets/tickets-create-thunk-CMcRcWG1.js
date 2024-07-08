180e6c92e0f313d1c5230712af368e5ae8545fa1
unknown <Marlou Dev>
2024-07-08 14:24:03 -0400

    added new build

---
+++ public/build/assets/tickets-create-thunk-CMcRcWG1.js
@@ -0,0 +1,1 @@
+import{s as o}from"./tickets-service-Lhq1CC4v.js";import{v as c}from"./page-CweAZ6u7.js";function a(){return async function(t,e){const r=e().tickets_create.form,s=await o(r);return t(c.actions.setForm({})),s}}export{a as t};
