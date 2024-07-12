2a5025cc9c4de7542cbc0d38ad1980e72f871a8a
unknown <Marlou Dev>
2024-07-12 14:56:49 -0400

    adjustment

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -57,7 +57,7 @@ class UserController extends Controller
                     ['call_type', '=', $user->agent_type == 'Warranty'?'CF-Warranty Claim':'Parts'],
                     // ['email_date', '<=', $twoDaysAgo]
                 ])
-                ->whereRaw('DATE_ADD(email_date, INTERVAL ? DAY) <= ?', [strval($days), $today])
+                ->whereRaw('DATE_ADD(email_date, INTERVAL ? DAY) <= ?', ['4', $today])
                 ->count();
 
                 $cases_due_today = Ticket::where([
