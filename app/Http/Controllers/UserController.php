169e0dd5adb8bd1ff2132b0da32f0c1257131ea6
unknown <Marlou Dev>
2024-07-12 14:24:44 -0400

    another adjustment

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -43,7 +43,7 @@ class UserController extends Controller
         if ($days >= 4) {
             $daysToAdd = 6 - $days;
         } else {
-            $daysToAdd = 6 - $days;
+            $daysToAdd = 2 - $days;
         }
         $two_overdue_cases = Carbon::now()->addDays($days)->toDateTimeString();
         if ($role_id == 5) {
