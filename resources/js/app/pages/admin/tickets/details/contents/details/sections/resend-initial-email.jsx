097af3d09f9b5e2a6339022aa061ec9cab0102af
unknown <Marlou Dev>
2024-07-22 12:13:51 -0400

    added email notification

--- resources/js/app/pages/admin/tickets/details/contents/details/sections/resend-initial-email.jsx
+++ resources/js/app/pages/admin/tickets/details/contents/details/sections/resend-initial-email.jsx
@@ -1,12 +1,13 @@
 import { parts_initial, warranty_initial } from "@/app/json/initial-templates";
 import Loading from "@/app/layouts/components/loading";
-import {  resend_email_templete_service } from "@/app/services/tickets-service";
+import { resend_email_templete_service } from "@/app/services/tickets-service";
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
-
+import { message } from "antd";
 export default function ResendInitialEmail() {
     const [loading, setLoading] = useState(false);
     const { ticket } = useSelector((state) => state.tickets);
+    const [messageApi, contextHolder] = message.useMessage();
 
     const warranty = warranty_initial(ticket);
     const parts = parts_initial(ticket);
@@ -21,23 +22,34 @@ export default function ResendInitialEmail() {
                 recipient: ticket.email,
             });
             setLoading(false);
+            messageApi.open({
+                type: "success",
+                content: "Sent Initial Email Successfully!",
+            });
         } catch (error) {
             setLoading(false);
+            messageApi.open({
+                type: "error",
+                content: "This is an error message",
+            });
         }
     }
     return (
-        <button
-            disabled={loading}
-            onClick={resend_email}
-            className="bg-purple-500 p-2 text-white  hover:bg-purple-600 flex items-center justify-center w-64"
-        >
-            {loading ? (
-                <Loading />
-            ) : (
-                <div className="p-1 w-full flex items-center justify-center">
-                    RESEND INITIAL EMAIL
-                </div>
-            )}
-        </button>
+        <>
+            {contextHolder}
+            <button
+                disabled={loading}
+                onClick={resend_email}
+                className="bg-purple-500 p-2 text-white  hover:bg-purple-600 flex items-center justify-center w-64"
+            >
+                {loading ? (
+                    <Loading />
+                ) : (
+                    <div className="p-1 w-full flex items-center justify-center">
+                        RESEND INITIAL EMAIL
+                    </div>
+                )}
+            </button>
+        </>
     );
 }
