bf31deb4f2f1d3f77fcb7c9097fedeb3bf19e521
unknown <Marlou Dev>
2024-08-02 10:11:54 -0400

    additional adjustmenmt

--- app/Http/Controllers/CasesLogController.php
+++ app/Http/Controllers/CasesLogController.php
@@ -22,6 +22,9 @@ class CasesLogController extends Controller
     {
         CasesLog::create($request->all());
         $logs = CasesLog::where('ticket_id', $request->ticket_id)->with('user')->get();
+        Ticket::where('id',$request->ticket_id)->update([
+            'is_reply'=>null
+        ]);
         $direct = DirectEmail::where('id', $request->ticket_id)->first();
         if ($direct) {
             $direct->update([
