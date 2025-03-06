c29dad34a7814846cc58605687e29a4655903252
Marlou Dev <marlou.dev.app@gmail.com>
2025-03-07 03:43:34 +0800

    revert backend

--- app/Http/Controllers/AppScriptController.php
+++ app/Http/Controllers/AppScriptController.php
@@ -13,8 +13,16 @@ class AppScriptController extends Controller
     public function get_warranty_unread_email(Request $request)
     {
 
-       
-        foreach ($request->all() as $value) {
+        $datas = $request->validate([
+            '*.ticket_id' => 'required',
+            '*.from' => 'required',
+            '*.to' => 'required',
+            '*.date' => 'required',
+            '*.threadId' => 'required',
+            '*.count' => 'required',
+        ]);
+
+        foreach ($datas as $value) {
             $ticket = Ticket::where('ticket_id', $value['ticket_id'])->first();
             if ($ticket) {
                 if ($value['from'] != 'support2@curtiscs.com') {
