b79f2aea45e2abf01d89d9392bce54615b296fc5
Marlou Dev <web@empireonegroup.com>
2026-01-30 23:12:21 +0800

    revert the adjustment

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -78,7 +78,7 @@ class UserController extends Controller
                     // ->where('call_type', $user->agent_type === 'Warranty'
                     //     ? 'CF-Warranty Claim'
                     //     : 'Parts')
-                    ->where('created_at', '>=', Carbon::now()->subMonths(5))
+                    ->where('created_at', '>=', Carbon::now()->subMonths(4))
                     ->get()
                     ->map(function ($t) use ($adjustDate) {
                         $t->email_date = $adjustDate($t->email_date);
