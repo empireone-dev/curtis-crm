b2fd61994307d88446cf5f91ae88fadcf39d3d55
Marlou Dev <web@empireonegroup.com>
2026-06-08 23:07:02 +0800

    added new adjustment

--- resources/js/app/pages/admin/product_registration/id/sections/product-registration-id-details-section.jsx
+++ resources/js/app/pages/admin/product_registration/id/sections/product-registration-id-details-section.jsx
@@ -111,6 +111,44 @@ export default function ProductRegistrationIDDetailsSection() {
                                 role="list"
                                 className="divide-y divide-gray-100 rounded-md border border-gray-200"
                             >
+                                {product?.bill_of_sale && <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+                                    <button
+                                        onClick={() =>
+                                            window.open(
+                                                product.bill_of_sale,
+                                                "_blank"
+                                            )
+                                        }
+                                        className="flex items-center justify-between w-full"
+                                    >
+                                        <div className="flex w-0 flex-1 items-center">
+                                            <PaperClipIcon
+                                                aria-hidden="true"
+                                                className="h-5 w-5 flex-shrink-0 text-gray-400"
+                                            />
+                                            <div className="ml-4 flex min-w-0 flex-1 gap-2">
+                                                <span className="truncate font-medium">
+                                                    {
+                                                        product.bill_of_sale.split(
+                                                            "/"
+                                                        )[5]
+                                                    }
+                                                </span>
+                                                <span className="flex-shrink-0 text-gray-400">
+                                                    2.4mb
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <div className="ml-4 flex-shrink-0">
+                                            <a
+                                                href="#"
+                                                className="font-medium text-indigo-600 hover:text-indigo-500"
+                                            >
+                                                Show
+                                            </a>
+                                        </div>
+                                    </button>
+                                </li>}
                                 {product?.prf?.map((res, i) => {
                                     return (
                                         <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
