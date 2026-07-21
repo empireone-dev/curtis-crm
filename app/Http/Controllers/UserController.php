22346977dcd31f5e28ea71a46bdc82e0d46186ba
Marlou Dev <web@empireonegroup.com>
2026-07-21 15:04:08 +0800

    added adjustment

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -76,7 +76,7 @@ class UserController extends Controller
                             ->whereNotNull('email')
                             // ->where('created_at', '>=', Carbon::parse('2025-05-01'))
                             // ->where('created_at', '>=', Carbon::now()->subMonths(13))
-                            ->whereBetween('created_at', [Carbon::parse('2025-06-01')->startOfDay(), Carbon::now()])
+                            ->where('created_at', '>=', Carbon::parse('2025-06-01')->startOfDay())
                             ->whereYear('created_at', '<>', 2024);
                     },
                     'directEmails' => function ($q) {
